6202b6aeebd247a9459e0a9da47952a7531be4e0
blazp04 <blaz.peric4@gmail.com>
2025-02-05 18:06:53 +0100

    .

---
+++ src/views/HomePage/HomePageView.vue
@@ -0,0 +1,8 @@
+<template>
+  <CityCard />
+</template>
+
+<script setup lang="ts">
+import { CityCard } from "./components/CityCard";
+</script>
+<style></style>
---
+++ src/views/HomePage/components/CityCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>Card Title</CardTitle>
+      <CardDescription>Card Description</CardDescription>
+    </CardHeader>
+    <CardContent>
+      Card Content
+    </CardContent>
+    <CardFooter>
+      Card Footer
+    </CardFooter>
+  </Card>
+</template>
+
+<script setup lang="ts">
+</script>
+<style></style>
