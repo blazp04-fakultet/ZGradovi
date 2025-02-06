21afca826122d4aeebda002fbb7aaf86b085f4e3
vucicj <jelena.vucic3@gmail.com>
2025-02-06 12:55:00 +0100

    .

--- src/views/HomePage/components/CityCard.vue
+++ src/views/HomePage/components/CityCard.vue
@@ -1,28 +1,30 @@
 <template>
-  <Card class="w-[25rem]  transition-shadow hover:drop-shadow-2xl drop-shadow-xl rounded-xg">
+  <Card
+    class="w-[25rem] transition-shadow hover:drop-shadow-2xl drop-shadow-xl rounded-xg"
+  >
     <CardContent class="flex flex-col p-0 rounded-xg">
-      <img class="w-full aspect-[16/11] pb-4 rounded-xg " :src="props.model.thumbnailURL" :alt="props.model.name">
+      <img
+        class="w-full aspect-[16/11] pb-4 rounded-xg"
+        :src="props.model.thumbnailURL"
+        :alt="props.model.name"
+      />
       <h3 class="text-2xl font-bold pl-3">{{ props.model.name }}</h3>
       <p class="text-cs pl-3 pb-3">{{ props.model.country }}</p>
     </CardContent>
     <CardFooter class="p-3">
-      <Button class="w-full">Nauči više</Button>
+      <CityDetails />
     </CardFooter>
   </Card>
 </template>
 
 <script setup lang="ts">
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from '@/components/ui/card'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
-import type { CityModel } from '@/domain/model/local/CityModel';
+import type { CityModel } from '@/domain/model/local/CityModel'
+import CityDetails from './CityDetails.vue'
 
 const props = defineProps<{
   model: CityModel
 }>()
-
 </script>
 <style></style>
---
+++ src/views/HomePage/components/CityDetails.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger
+      class="text-white border-2 border-black bg-transparent px-6 py-2 rounded-md bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-black"
+    >
+      Nauči više
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogDescription class="flex items-center space-x-4 w-full">
+          <img src="" alt="profile image" class="w-16 h-16 rounded-full" />
+
+          <div class="flex flex-col space-y-4 w-full">
+            <div>
+              <Label for="grad">Ime grada</Label>
+              <Separator class="my-4 w-full" label="" />
+            </div>
+            <div>
+              <Label for="grad">Opis</Label>
+              <Separator class="my-4 w-full" label="" />
+            </div>
+            <div>
+              <Label for="grad">Država</Label>
+              <Separator class="my-4 w-full" label="" />
+            </div>
+            <div>
+              <Label for="grad">Poštanski broj</Label>
+              <Separator class="my-4 w-full" label="" />
+            </div>
+            <div>
+              <Label for="grad">Broj stanovnika</Label>
+              <Separator class="my-4 w-full" label="" />
+            </div>
+          </div>
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+</template>
