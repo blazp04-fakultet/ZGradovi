067283eec4fe93b17b01b317ed513f75e719a2c3
blazp04 <blaz.peric4@gmail.com>
2025-02-06 10:07:25 +0100

    .

--- src/domain/repository/AutentificationRepository.ts
+++ src/domain/repository/AutentificationRepository.ts
@@ -1,5 +1,5 @@
-import apiConfig from "@/config/AxiosConfig";
-import type  SignupRequestModel  from "../model/request/SignupRequestModel.ts";
+import apiConfig from '@/config/AxiosConfig'
+import type SignupRequestModel from '../model/request/SignupRequestModel.ts'
 async function fetchUserData(): Promise<UserResponseModel> {}
 async function login(email: string, password: string): Promise<string> {
   try {
@@ -10,15 +10,15 @@ async function login(email: string, password: string): Promise<string> {
     const response = await apiConfig.post('users/login', payload)
     const data = response.data
     const token = data.token
-  } catch (e) {
     return token
+  } catch (e) {
     throw e
   }
 }
-async function signup(model:SignupRequestModel): Promise<string> {
-const response = await apiConfig.post('users/signup', model)
-const data = await response.data
-const token = data.token
-return token
+async function signup(model: SignupRequestModel): Promise<string> {
+  const response = await apiConfig.post('users/signup', model)
+  const data = await response.data
+  const token = data.token
+  return token
 }
 export { fetchUserData, login, signup }
